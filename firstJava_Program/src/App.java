91ec585f79e8444c3e132f93ef33d58a0085bf86
bryanathll <bryannatanael77@gmail.com>
2024-03-21 21:25:26 +0700

    InitialCommit

--- firstJava_Program/src/App.java
+++ firstJava_Program/src/App.java
@@ -5,17 +5,17 @@ public class App {
         String Nama = "UPB yess";
         String NAMA = "Upn josss";
 
-        int nilai = 80;
-        double Nilai = 90.8;
+        int nilai_pertama = 80;
+        double nilai_kedua = 90.8;
         char gender = 'L';
 
         System.out.println("Data pada variabel nama = "+nama);
         System.out.println("Data pada variabel nama = "+Nama);
         System.out.println("Data pada variabel nama = "+NAMA);
 
-        System.out.println(nilai);
-        System.out.println(Nilai);
-        System.out.println(gender);
+        System.out.println("Data pada variabel nilai pertama adalah: "+nilai_pertama);
+        System.out.println("Data pada variabel nilai kedua adalah: "+nilai_kedua);
+        System.out.println("Data pada gender adalah: "+gender);
 
     }
 
