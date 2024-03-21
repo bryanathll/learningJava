c6c1f48444fd1010b0f92f25feb851d425719258
bryanathll <bryannatanael77@gmail.com>
2024-03-21 21:48:47 +0700

    InitialCommit

--- firstJava_Program/src/Task.java
+++ firstJava_Program/src/Task.java
@@ -8,9 +8,35 @@ public class Task {
          hasil penjumlahan = bilangan1+bilangan2
          tampilkan ke ouput hasil penjumlahan*/
 
-        int bilangan1 = 6;
-        int bilangan2 = 5;
 
-        System.out.println( bilangan1 + bilangan2);
+        // ============ penjumlahan ============
+        int bilangan1, bilangan2, hasil_penjumlahan;
+        bilangan1 = 6;
+        bilangan2 = 5;
+        hasil_penjumlahan = bilangan1 + bilangan2;
+        
+        System.out.println( "hasil dari penjumlhan bilangan1 dan bilangan2 adalah:" + hasil_penjumlahan);
+        // ============ penjumlahan end============
+
+        
+        // ============ perkalian ============
+        int bill1, bill2, hasil_perkalian;
+        bill1 = 4;
+        bill2 = 6;
+        hasil_perkalian = bill1 * bill2;
+
+        System.out.println("hasil perkalian bill1 dan bill2 adalah: "+hasil_perkalian);
+        // ============ perkalian end============
+
+        
+
+        // ============ pembagian ============
+        double bil1, bil2, hasil_pembagian;
+        bil1 = 18;
+        bil2 = 3;
+        hasil_pembagian = bil1 * bil2;
+
+        System.out.println("hasil pembagian bill1 dan bill2 adalah: "+hasil_pembagian);
+        // ============ pembagian end============
     }
 }
