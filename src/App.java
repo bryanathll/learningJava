cfc74ff9f1a1b13b7e67a8be78009c14e25cf54c
bryanathll <bryannatanael77@gmail.com>
2024-03-21 21:15:34 +0700

    InitialCommit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        //deklarasi variabel dan tipe data
+        String nama = "UPB";
+        String Nama = "UPB yess";
+        String NAMA = "Upn josss";
+
+        System.out.println("Data pada variabel nama = "+nama);
+        System.out.println("Data pada variabel nama = "+Nama);
+        System.out.println("Data pada variabel nama = "+NAMA);
+
+    }
+
+}
