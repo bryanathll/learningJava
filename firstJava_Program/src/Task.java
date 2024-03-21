af5720ff9f229d871715ff1d37d416df0c654038
bryanathll <bryannatanael77@gmail.com>
2024-03-21 21:35:04 +0700

    InitialCommit

---
+++ firstJava_Program/src/Task.java
@@ -0,0 +1,16 @@
+public class Task {
+    public static void main(String[] args) {
+        /* deklarasi variabel dan tipe data untuk
+         bilangan1, bilangan2, hasil penjumlahan
+         bilangan1 = 6
+         bilangan2 = 5
+
+         hasil penjumlahan = bilangan1+bilangan2
+         tampilkan ke ouput hasil penjumlahan*/
+
+        int bilangan1 = 6;
+        int bilangan2 = 5;
+
+        System.out.println( bilangan1 + bilangan2);
+    }
+}
